2c9092d522595d8402bb4f834514f850bbcf9980
RAJIV KUMAR <114786589+Rajivkrrao@users.noreply.github.com>
2022-11-25 01:05:18 +0530

    Add files via upload

---
+++ DeleteRecordsOfAList/DeleteRecordsOfAList/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteRecordsOfAList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientContext ctx = new ClientContext("http://localhost:25000");
+
+            Web web = ctx.Web;
+
+            List lst = web.Lists.GetByTitle("CourseCategories");
+
+            CamlQuery query = new CamlQuery();
+
+            ListItemCollection lic = lst.GetItems(query);
+
+            ctx.Load(lic);
+
+            ctx.ExecuteQuery();
+
+            foreach (ListItem li in lic.ToList())
+            {
+                li.DeleteObject();
+            }
+
+            ctx.ExecuteQuery();
+            Console.WriteLine("All records have been deleted..");
+            Console.ReadKey();
+        }
+    }
+}
