43a9e7179aef7f73522cecae4920a2b4af6d6307
RAJIV KUMAR <114786589+Rajivkrrao@users.noreply.github.com>
2022-11-25 00:56:58 +0530

    Add files via upload

---
+++ GetListOfLists/GetListOfLists/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace GetListOfLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientContext ctx = new ClientContext("http://localhost:25000");
+
+            Web web = ctx.Web;
+
+            ctx.Load(web);
+
+             ctx.Load(web.Lists);
+
+            //ctx.Load(web, wc => wc.Lists.Where(lic => lic.ItemCount >= 1));
+
+            // ctx.Load(web, wc => wc.Lists.Where(list => list.BaseTemplate == 105));
+
+            ctx.Load(web, wc => wc.Lists.Where(list => list.BaseType == BaseType.GenericList));
+
+
+
+            ctx.ExecuteQuery();
+
+            foreach(var list in web.Lists)
+            {
+
+                Console.WriteLine("List Title is : {0}",list.Title);
+            }
+
+            Console.WriteLine("Lists Count is : {0}",web.Lists.Count);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GetListOfLists/GetListOfLists/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GetListOfLists")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GetListOfLists")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1d988249-7baf-41c5-a9bc-cdfbe22386f8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
