fffe3fbab6f4e310ac6ea5d2eec0cfbad4d16f3e
RAJIV KUMAR <114786589+Rajivkrrao@users.noreply.github.com>
2022-11-25 01:04:28 +0530

    Add files via upload

---
+++ GetListOfSubSites/GetListOfSubSites/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetListOfSubSites
+{
+    class Program
+    {
+        static string ParentSitePath = "http://localhost:25000";
+        static void Main(string[] args)
+        {
+
+            GetListOfSubSites(ParentSitePath);
+            Console.ReadKey();
+        }
+
+        public static void GetListOfSubSites(string url)
+        {
+            try
+            {
+                ClientContext ctx = new ClientContext(url);
+                Web web = ctx.Web;
+                ctx.Load(web, ws => ws.Webs, ws => ws.Title);
+                ctx.ExecuteQuery();
+
+                foreach (Web ChildWeb in web.Webs)
+                {
+                    string ChildWebSitePath = ParentSitePath + ChildWeb.ServerRelativeUrl;
+                    GetListOfSubSites(ChildWebSitePath);
+                    Console.WriteLine(ChildWebSitePath + "\n" + ChildWeb.Title);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error is {0}", ex.Message);
+            }
+        }
+    }
+}
