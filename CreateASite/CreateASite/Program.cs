c2682921a4d16055b601974aab0bda311a0ae45b
RAJIV KUMAR <114786589+Rajivkrrao@users.noreply.github.com>
2022-11-25 01:06:10 +0530

    Add files via upload

---
+++ CreateASite/CreateASite/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateASite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:25000";
+            string Description = "Created this site through CSOM Approach.";
+            int Language = 1033;
+            string Title = "NewSite";
+            string SiteUrl = "NewSite";
+            bool SitePermissions = true;
+            string SiteTemplate = "STS#0";
+
+            ClientContext ctx = new ClientContext(url);
+            Web web = ctx.Web;
+
+            WebCreationInformation wci = new WebCreationInformation();
+            wci.Description = Description;
+            wci.Language = Language;
+            wci.Title = Title;
+            wci.Url = SiteUrl;
+            wci.UseSamePermissionsAsParentSite = SitePermissions;
+            wci.WebTemplate = SiteTemplate;
+
+            Web NewWeb = web.Webs.Add(wci);
+
+            ctx.Load(
+                NewWeb,
+                w => w.Title,
+                w => w.Description);
+
+            ctx.ExecuteQuery();
+
+            Console.WriteLine("Title: {0} Description: {1}", NewWeb.Title, NewWeb.Description);
+            Console.ReadKey();
+        }
+    }
+}
